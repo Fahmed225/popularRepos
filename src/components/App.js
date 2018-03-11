9caf424baad0bcb54498397828ea1257e0c53db2
Fahmed225 <fadbz08@gmail.com>
2018-03-11 18:37:41 -0400

    added Battle component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var Popular = require('./Popular');
+var Battle = require('./Battle');
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <Popular />
+//       <Popular />
+        <Battle />
       </div>
     )
   }
