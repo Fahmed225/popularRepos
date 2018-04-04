9d080c052bf47ad104793f2ffc74639818f6f3d0
Fahmed225 <fadbz08@gmail.com>
2018-04-04 12:02:57 -0400

    blah

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,15 +3,41 @@ var queryString = require('query-string');
 var api = require('../utils/api');
 
 class Results extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
   componentDidMount() {
     var players = queryString.parse(this.props.location.search);
     
     api.battle([players.playerOneName, players.playerTwoName])
       .then(function (results) {
-        console.log(results);
-      });
+        if (results === null) {
+          return this.setState(function() {
+            return {
+              error: 'Looks like there was an error. Check that both users exist.',
+              loading: false
+            }
+          });
+        }
+      
+      this.setState(function {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+      })
+    }.bind(this));
+    
   }
-  
   render() {
     return(
       <div>Results</div>
